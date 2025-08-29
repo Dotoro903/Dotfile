dade4e5229bfa51c9ea5a12aec5855d81a7d9e0e
Dotoro903 <alderwood903@gmail.com>
2025-08-30 00:03:02 +0900

    feat(neovim): configure formatter for shell scripts

--- nvim/.config/nvim/lua/Dotoro/plugins/formatting.lua
+++ nvim/.config/nvim/lua/Dotoro/plugins/formatting.lua
@@ -1,30 +1,35 @@
 return {
-    "stevearc/conform.nvim",
-    config = function()
-        require("conform").setup({
+	"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
 
-            format_on_save = { lsp_format = "fallback", timeout_ms = 2000 },
+			-- format_on_save = { lsp_format = "fallback", timeout_ms = 2000 },
+			format_on_save = nil,
 
-            formatters_by_ft = {
-                lua        = { "stylua" },
-                -- Conform will run multiple formatters sequentially
-                python     = { "isort", "black" },
-                -- You can customize some of the format options for the filetype (:help conform.format)
-                rust       = { "rustfmt", lsp_format = "fallback" },
-                -- Conform will run the first available formatter
-                javascript = { "prettierd", "prettier", stop_after_first = true },
-                typescript = { "prettierd", "prettier"},
-                json       = { "prettierd", "prettier"},
-                html       = { "prettierd", "prettier"},
-                css        = { "prettierd", "prettier"},
-                markdown   = { "prettierd", "prettier"},
-                yaml       = { "prettierd", "prettier"},
+			formatters_by_ft = {
+                -- stylua: ignore start
+				lua        = { "stylua" },
+				python     = { "isort", "black" },
+				rust       = { "rustfmt", lsp_format = "fallback" },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier" },
+				json       = { "prettierd", "prettier" },
+				html       = { "prettierd", "prettier" },
+				css        = { "prettierd", "prettier" },
+				markdown   = { "prettierd", "prettier" },
+				yaml       = { "prettierd", "prettier" },
 
-                java = { "clang-format" },
+				sh         = { "shfmt" },
+				bash       = { "shfmt" },
+				zsh        = { "shfmt" },
 
-                c          = { "clang-format" },
-                cpp        = { "clang-format" },
-            },
-        })
-    end,
+				java       = { "clang-format" },
+
+				c          = { "clang-format" },
+				cpp        = { "clang-format" },
+
+				-- stylua: ignore end
+			},
+		})
+	end,
 }
