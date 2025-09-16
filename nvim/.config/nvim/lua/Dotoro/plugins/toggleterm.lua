503be9b2674f33fbb940904ac22dcd946c43c3a6
Dotoro903 <alderwood903@gmail.com>
2025-09-16 17:09:21 +0900

    chore(nvim): tweaks

--- nvim/.config/nvim/lua/Dotoro/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/Dotoro/plugins/toggleterm.lua
@@ -1,8 +1,10 @@
 return {
-    'akinsho/toggleterm.nvim',
-    version = "*",
-    opts = {},
-    keys = {
-        { '<leader>t', ':ToggleTerm<CR>', desc = "Toggle terminal" }
-    }
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	opts = {},
+	keys = {
+		{ "<C-BS>", ":ToggleTerm<CR>", desc = "Toggle terminal" },
+		{ "<C-BS>", [[<C-\><C-n><cmd>ToggleTerm<CR>]], desc = "Toggle terminal", mode = "t" },
+		{ "<Esc>", [[<C-\><C-n>]], desc = "Terminal -> Normal", mode = "t" },
+	},
 }
