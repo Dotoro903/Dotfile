96c510c772a022bf7d79a51105592924af1f1666
Dotoro903 <alderwood903@gmail.com>
2025-09-19 11:10:18 +0900

    feat(nvim): Set to have different indenent depending on the file type

--- nvim/.config/nvim/lua/Dotoro/core/options.lua
+++ nvim/.config/nvim/lua/Dotoro/core/options.lua
@@ -10,9 +10,23 @@ vim.opt.guicursor = {
   "o:hor50",
 }
 
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "javascript","javascriptreact","typescript","typescriptreact",
+    "json","jsonc","css","scss","less","html",
+    "markdown","mdx","yaml","vue","svelte","graphql"
+  },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+  end,
+})
+
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
