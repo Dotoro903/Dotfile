65898ac086ebe37fe26bc8a00d09359af989907d
Dotoro903 <alderwood903@gmail.com>
2025-12-19 22:06:43 +0900

    chore(nvim): keymap tweak

--- nvim/.config/nvim/lua/Dotoro/plugins/git-stuffs.lua
+++ nvim/.config/nvim/lua/Dotoro/plugins/git-stuffs.lua
@@ -54,8 +54,8 @@ return {
 					col = 1,
 				},
 
-				vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {}),
-				vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {}),
+				vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { noremap = true, silent = true }),
+				vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { noremap = true, silent = true }),
 			})
 		end,
 	},
