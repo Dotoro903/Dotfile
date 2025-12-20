5e18cb1b20acf5b946e8dfa3dd461b28707feee5
Dotoro903 <alderwood903@gmail.com>
2025-12-20 16:01:21 +0900

    add(nvim): treesitter textobject typescript

---
+++ nvim-treesitter/.local/share/nvim/lazy/nvim-treesitter-textobjects/queries/typescript/textobjects.scm
@@ -0,0 +1,110 @@
+; inherits: ecma
+
+;; const/let/var x = <rhs>
+(variable_declarator
+  value: (_) @assignment.rhs)
+
+;; x = <rhs>   (includes this.head = this.head.prev)
+(assignment_expression
+  right: (_) @assignment.rhs)
+
+(interface_declaration) @class.outer
+
+(interface_declaration
+  body: (interface_body
+    .
+    "{"
+    .
+    (_) @_start @_end
+    _? @_end
+    .
+    "}"
+    (#make-range! "class.inner" @_start @_end)))
+
+(type_alias_declaration) @class.outer
+
+(type_alias_declaration
+  value: (object_type
+    .
+    "{"
+    .
+    (_) @_start @_end
+    _? @_end
+    .
+    "}"
+    (#make-range! "class.inner" @_start @_end)))
+
+(enum_declaration) @class.outer
+
+(enum_declaration
+  body: (enum_body
+    .
+    "{"
+    .
+    (_) @_start @_end
+    _? @_end
+    .
+    "}"
+    (#make-range! "class.inner" @_start @_end)))
+
+; type, interface items as @parameter
+; 1. parameter.inner
+(property_signature) @parameter.inner
+
+; 2. parameter.outer: Only one element, no comma
+(object_type
+  .
+  (property_signature) @parameter.outer .)
+
+(interface_body
+  .
+  (property_signature) @parameter.outer .)
+
+; 3. parameter.outer: Comma/semicolon before or after
+([
+  (object_type
+    "," @_start
+    .
+    (property_signature) @_end)
+  (interface_body
+    "," @_start
+    .
+    (property_signature) @_end)
+]
+  (#make-range! "parameter.outer" @_start @_end))
+
+([
+  (object_type
+    (property_signature) @_start
+    .
+    "," @_end)
+  (interface_body
+    (property_signature) @_start
+    .
+    "," @_end)
+]
+  (#make-range! "parameter.outer" @_start @_end))
+
+([
+  (object_type
+    ";" @_start
+    .
+    (property_signature) @_end)
+  (interface_body
+    ";" @_start
+    .
+    (property_signature) @_end)
+]
+  (#make-range! "parameter.outer" @_start @_end))
+
+([
+  (object_type
+    (property_signature) @_start
+    .
+    ";" @_end)
+  (interface_body
+    (property_signature) @_start
+    .
+    ";" @_end)
+]
+  (#make-range! "parameter.outer" @_start @_end))
