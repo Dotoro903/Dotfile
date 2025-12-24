b9a755c64476684f914f5abdbe94e0dc0361eb13
Dotoro903 <alderwood903@gmail.com>
2025-12-24 13:56:37 +0900

    chore(nvim): disable underline highlight

--- nvim/.config/nvim/lua/Dotoro/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/Dotoro/plugins/indent-blankline.lua
@@ -1,8 +1,14 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    
-    opts = {},
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	---@module "ibl"
+	---@type ibl.config
+
+  opts = {
+    scope = {
+      enabled = true,
+      show_start = false,
+      show_end = false,
+    },
+  },
 }
