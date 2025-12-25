15bdd12aad5ff51e22bf490f72ec19c05417a598
Dotoro903 <alderwood903@gmail.com>
2025-12-25 14:28:20 +0900

    feat(nvim): telescope tweaks, and add buffers shortcut

--- nvim/.config/nvim/lua/Dotoro/plugins/telescope.lua
+++ nvim/.config/nvim/lua/Dotoro/plugins/telescope.lua
@@ -1,14 +1,36 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+		vim.keymap.set("n", "<leader>gg", builtin.git_files, { desc = "Telescope git files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+		vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Telescope live grep" })
 
-    config = function()
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>gg', builtin.find_files, { desc = 'Telescope git files' })
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-    end,
+		require("telescope").setup({
+			defaults = {},
+			pickers = {
+				find_files = {
+					theme = "ivy",
+				},
+				git_files = {
+					theme = "ivy",
+				},
+				live_grep = {
+					theme = "ivy",
+				},
+				buffers = {
+					theme = "ivy",
+					initial_mode = "normal",
+					mappings = {
+						n = {
+							["dd"] = require("telescope.actions").delete_buffer,
+						},
+					},
+				},
+			},
+		})
+	end,
 }
-
-
