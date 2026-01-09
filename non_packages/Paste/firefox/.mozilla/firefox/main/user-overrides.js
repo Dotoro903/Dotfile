65e2df3ca34eec8b2a8b8c8a6839144e4d03a11a
Dotoro903 <alderwood903@gmail.com>
2026-01-09 18:08:57 +0900

    firefox

---
+++ non_packages/Paste/firefox/.mozilla/firefox/main/user-overrides.js
@@ -0,0 +1,21 @@
+user_pref('ui.key.menuAccessKey', 0);
+user_pref('ui.key.menuAccessKeyFocuses', false);
+
+user_pref('privacy.clearOnShutdown.cache', false);
+user_pref('privacy.clearOnShutdown.cookies', false);
+user_pref('privacy.clearOnShutdown.downloads', true);
+user_pref('privacy.clearOnShutdown.formdata', true);
+user_pref('privacy.clearOnShutdown.history', true);
+user_pref('privacy.clearOnShutdown.offlineApps', true);
+user_pref('privacy.clearOnShutdown.sessions', false);
+user_pref('privacy.clearOnShutdown.siteSettings', false);
+
+user_pref('privacy.clearOnShutdown_v2.cookiesAndStorage', false);
+user_pref('privacy.clearOnShutdown_v2.browsingHistoryAndDownloads', true);
+user_pref('privacy.clearOnShutdown_v2.cache', false);
+user_pref('privacy.clearOnShutdown_v2.downloads', true);
+user_pref('privacy.clearOnShutdown_v2.formdata', true);
+user_pref('privacy.clearOnShutdown_v2.historyFormDataAndDownloads', true);
+user_pref('privacy.clearOnShutdown_v2.siteSettings', false);
+
+user_pref("signon.rememberSignons", false);
---
+++ non_packages/Paste/firefox/.mozilla/firefox/secure/user-overrides.js
@@ -0,0 +1,13 @@
+user_pref('ui.key.menuAccessKey', 0);
+user_pref('ui.key.menuAccessKeyFocuses', false);
+
+user_pref('privacy.clearOnShutdown.cache', true);
+user_pref('privacy.clearOnShutdown.cookies', true);
+user_pref('privacy.clearOnShutdown.downloads', true);
+user_pref('privacy.clearOnShutdown.formdata', true);
+user_pref('privacy.clearOnShutdown.history', true);
+user_pref('privacy.clearOnShutdown.offlineApps', true);
+user_pref('privacy.clearOnShutdown.sessions', true);
+user_pref('privacy.clearOnShutdown.siteSettings', true);
+
+user_pref("signon.rememberSignons", false);
