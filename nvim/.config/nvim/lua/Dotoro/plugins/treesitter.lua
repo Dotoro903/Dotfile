88d742e3d4b526ab3b6f1806d1427548c224dbef
Dotoro903 <alderwood903@gmail.com>
2026-01-10 12:09:35 +0900

    chore(nvim): trim treesitter and replace context to treesitter context

--- nvim/.config/nvim/lua/Dotoro/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/Dotoro/plugins/treesitter.lua
@@ -1,72 +1,99 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("treesitter-context").setup({
+				enable = true,
+				multiwindow = false, -- Enable multiwindow support.
+				max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+				min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+				line_numbers = true,
+				multiline_threshold = 20, -- Maximum number of lines to show for a single context
+				trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+				mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+				-- Separator between context and content. Should be a single character string, like '-'.
+				-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+				separator = "─",
+				zindex = 20, -- The Z-index of the context window
+				on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+			})
+		end,
 	},
-	priority = 1000,
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"c",
-				"cpp",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"javascript",
-				"typescript",
-				"python",
-				"rust",
-				"go",
-				"html",
-				"css",
-				"json",
-				"yaml",
-				"markdown",
-				"tsx",
-			},
-			sync_install = false,
-			modules = {},
-			ignore_install = {},
-
-			auto_install = true,
-
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = {
-				enable = true,
-			},
-
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "gnn",
-					node_incremental = "grn",
-					scope_incremental = "grc",
-					node_decremental = "grm",
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		priority = 1000,
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"c",
+					"cpp",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"javascript",
+					"typescript",
+					"python",
+					"rust",
+					"go",
+					"html",
+					"css",
+					"json",
+					"yaml",
+					"markdown",
+					"tsx",
 				},
-			},
+				sync_install = false,
+				modules = {},
+				ignore_install = {},
 
-			textobjects = {
-				select = {
+				auto_install = true,
+
+				highlight = {
 					enable = true,
-					lookahead = true,
+					additional_vim_regex_highlighting = false,
+				},
+				indent = {
+					enable = true,
+				},
+
+				incremental_selection = {
+					enable = true,
 					keymaps = {
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-
-						["ar"] = "@block.outer",
-						["ir"] = "@block.inner",
-
-						["av"] = "@assignment.rhs",
-						["iv"] = "@assignment.rhs",
+						init_selection = "gnn",
+						node_incremental = "grn",
+						scope_incremental = "grc",
+						node_decremental = "grm",
 					},
 				},
-			},
-		})
-	end,
+
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+
+							["ar"] = "@block.outer",
+							["ir"] = "@block.inner",
+
+							["av"] = "@assignment.rhs",
+							["iv"] = "@assignment.rhs",
+						},
+					},
+				},
+			})
+		end,
+	},
 }
